73053069804e15a297f36d9e61c666ae2a0b2ea8
Kyle <115855010+BjorkPimney@users.noreply.github.com>
2025-09-09 17:31:24 -0400

    Lab 1 - Javadoc comments and documentation practice. Meant to upload last night but forgot.

--- src/Lab1/BankAccount.java
+++ src/Lab1/BankAccount.java
@@ -1,25 +1,53 @@
 package Lab1;
 
+/**
+ * A new class called BankAccount to facilitate these banking methods
+ * @author Kyle Leahy
+ * @version 1.0
+ */
 public class BankAccount {
 
-    private double balance;
+    private double balance; //Private primitive type to be
+                            //used for arithmetic
 
+    /**
+     * Method calculates a deposit transaction based off 'balance'
+     * @param amount
+     */
     public void deposit(double amount) {
         balance = balance + amount;
     }
 
+    /**
+     * Method calculates a withdrawal transaction also using 'balance'
+     * @param amount
+     */
     public void withdrawal(double amount) {
         balance = balance - amount;
     }
 
+    /**
+     * Method retrieves the balance of the bank account in a 'getBalance' method
+     * @return Returns the balance of the bank account
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Method calculates the interest of the given bank account
+     * at a fixed value of 5% APY
+     */
     private void calculateInterest() {
-        balance = balance * 1.05;
+        balance = balance * 1.05; // <- 5% APY set value
     }
 
+    /**
+     * Main function creating 2 new BankAccount objects for Bob and Jane
+     * As well depositing and withdrawing cash to ensure
+     * after printing the balance, the methods work properly
+     * @param args
+     */
     public static void main(String args[]) {
         BankAccount bobsAccount = new BankAccount();
         BankAccount janesAccount = new BankAccount();
