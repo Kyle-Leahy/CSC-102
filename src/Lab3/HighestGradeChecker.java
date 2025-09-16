dc2e191f5c01ed3a504eeb901a0df31d74744c29
KyleLeahy <kll37@sunyorange.edu>
2025-09-16 15:41:58 -0400

    Lab3 started but not done at all

---
+++ src/Lab3/HighestGradeChecker.java
@@ -0,0 +1,20 @@
+package Lab3;
+import javax.swing.*;
+
+public class HighestGradeChecker {
+    public static void main(String[] args) {
+        boolean Status = true;
+        int grade;
+        int highestSoFar = 0;
+        String inquiry;
+        do{
+            inquiry = JOptionPane.showInputDialog("Enter your top 10 grades to see them averaged and evaluated.");15
+            grade = Integer.parseInt(inquiry);
+            if(grade > highestSoFar) {
+                grade = highestSoFar;
+                System.out.println(grade);
+                //Status = true;
+            }
+        }while(Status);
+    }
+}
