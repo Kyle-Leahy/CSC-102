0048a15bfa701aaf177fbe332a3a5c74740e9c74
KyleLeahy <kll37@sunyorange.edu>
2025-09-17 14:50:19 -0400

    Lab3 progress to be continued

--- src/Lab3/HighestGradeChecker.java
+++ src/Lab3/HighestGradeChecker.java
@@ -1,20 +1,31 @@
 package Lab3;
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class HighestGradeChecker {
     public static void main(String[] args) {
-        boolean Status = true;
-        int grade;
-        int highestSoFar = 0;
+        int answer;
+        double grade, gradeAverage, j = 1, highestSoFar = 0, lowestSoFar = 100;
         String inquiry;
-        do{
-            inquiry = JOptionPane.showInputDialog("Enter your top 10 grades to see them averaged and evaluated.");
+        ArrayList<Double> checker = new ArrayList<>();
+        String gradeAmount = JOptionPane.showInputDialog(null,"How many grades are you trying to enter?");
+        answer = Double.parseDouble(gradeAmount); // need to fix to doubles or use scanner
+        for (int i = 1; i < answer; i++) {
+            inquiry = JOptionPane.showInputDialog("Enter your top " + answer + " grades to see them averaged and evaluated.");
             grade = Integer.parseInt(inquiry);
+            checker.add(grade);
             if(grade > highestSoFar) {
-                grade = highestSoFar;
-                System.out.println(grade);
-                //Status = true;
+                highestSoFar = grade;
+                //JOptionPane.showMessageDialog(null,highestSoFar);
+            }else if(grade < lowestSoFar) {
+                lowestSoFar = grade;
             }
-        }while(Status);
+            j++;
+        }
+        gradeAverage = j/answer;
+        JOptionPane.showMessageDialog(null,"The highest grade so far is: " + highestSoFar);
+        JOptionPane.showMessageDialog(null,"The lowest grade so far is: " + lowestSoFar);
+        JOptionPane.showMessageDialog(null,"The average of the grades are: " + gradeAverage);
+
     }
 }
