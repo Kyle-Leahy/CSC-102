6f813afa3aa337583a7fb89d22282e4da5cb6ea9
KyleLeahy <kll37@sunyorange.edu>
2025-09-19 14:42:53 -0400

    Lab3 works flawlessly, just need to format it and make it A++.

--- src/Lab3/HighestGradeChecker.java
+++ src/Lab3/HighestGradeChecker.java
@@ -4,28 +4,26 @@ import java.util.ArrayList;
 
 public class HighestGradeChecker {
     public static void main(String[] args) {
-        int answer;
-        double grade, gradeAverage, j = 1, highestSoFar = 0, lowestSoFar = 100;
+        double total = 0, grade, gradeAverage, j = 1, highestSoFar = 0, lowestSoFar = 100;
         String inquiry;
         ArrayList<Double> checker = new ArrayList<>();
         String gradeAmount = JOptionPane.showInputDialog(null,"How many grades are you trying to enter?");
-        answer = Double.parseDouble(gradeAmount); // need to fix to doubles or use scanner
-        for (int i = 1; i < answer; i++) {
+        int answer = Integer.parseInt(gradeAmount);
+        for (int i = 0; i < answer; i++) {
             inquiry = JOptionPane.showInputDialog("Enter your top " + answer + " grades to see them averaged and evaluated.");
             grade = Integer.parseInt(inquiry);
             checker.add(grade);
+            total = total + grade;
+            j++;
             if(grade > highestSoFar) {
                 highestSoFar = grade;
-                //JOptionPane.showMessageDialog(null,highestSoFar);
             }else if(grade < lowestSoFar) {
                 lowestSoFar = grade;
             }
-            j++;
         }
-        gradeAverage = j/answer;
+        gradeAverage = (total/j);
         JOptionPane.showMessageDialog(null,"The highest grade so far is: " + highestSoFar);
         JOptionPane.showMessageDialog(null,"The lowest grade so far is: " + lowestSoFar);
         JOptionPane.showMessageDialog(null,"The average of the grades are: " + gradeAverage);
-
     }
 }
