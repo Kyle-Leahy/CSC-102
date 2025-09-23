3715aa591e266b71665edde206f96b7e1c1313e5
Bjork <kyleleahy68@gmail.com>
2025-09-23 10:20:04 -0400

    Lab3 File reader is done, was running the wrong program for a good 5 minutes wondering why my changes weren't doing anything. Pretty straight forward.

--- src/Lab3/GradeEvaluationFromFile.java
+++ src/Lab3/GradeEvaluationFromFile.java
@@ -6,49 +6,29 @@ import java.util.Scanner;
 
 //Same thing don't feel as though javadoc is necessary as all the methods are the same just different input means.
 public class GradeEvaluationFromFile {
-    public static File numbers = new File("numbers.txt");
-    public static Scanner in;
-    static {
-        try {in = new Scanner(numbers);}
-        catch (FileNotFoundException Error) {throw new RuntimeException(Error.getMessage());}
-    }
     public static double grade, avg, j = 1, total = 0, highestSoFar = 0, lowestSoFar = 100;
     public static int answer;
-    public static String inquiry, formatAvg;
-    public static void setAnswer(){
-        System.out.println("How many grades are you trying to enter?");
-        answer = in.nextInt();
-    }
-    public static void setGrade() {
-        for (int i = 0; i < answer; i++) {
-            System.out.println("Enter your top " + answer + " grades to see them averaged and evaluated.");
-            grade = in.nextDouble();
-            if (grade > highestSoFar) {highestSoFar = grade;}
-            else if (grade < lowestSoFar) {lowestSoFar = grade;}
-            GradeEvaluation.checker.add(grade);
-            total = total + grade;
-            j++;
-        }
-    }
-    public static double getHighestSoFar(){
-        return highestSoFar;
-    }
-    public static double getLowestSoFar(){
-        return lowestSoFar;
-    }
-    public static String getAverage(){
-        avg = (total/j);
-        formatAvg = String.format("%.2f",avg);
-        return formatAvg;
-    }
+    public static String formatAvg;
     public static ArrayList<Double> checker = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("~~Kyle Leahy~~\n~~CSC-102H~~\n~~9/18/2025~~");
-        setAnswer();
-        setGrade();
-        System.out.println("The highest grade so far is: " + GradeEvaluationFromFile.getHighestSoFar());
-        System.out.println("The lowest grade so far is: " + GradeEvaluationFromFile.getLowestSoFar());
-        System.out.println("The average of the grades are: " + GradeEvaluationFromFile.getAverage());
-
+        File answers = new File("src/Lab3/numbers.txt"); //local path of file.
+        try (Scanner reader = new Scanner(answers)) { // Reading in file to "reader".
+            while(reader.hasNextLine()) {
+                grade = Integer.parseInt(reader.nextLine());
+                if (grade > highestSoFar){highestSoFar = grade;} // Calculations of highest
+                else if (grade < lowestSoFar) {lowestSoFar = grade;} // and lowest
+                GradeEvaluation.checker.add(grade); // Expandable ArrayList
+                total = total + grade;
+                j++;
+            }
+            avg = (total/j);
+            formatAvg = String.format("%.2f",avg);
+            System.out.println("~~Kyle Leahy~~\n~~CSC-102H~~\n~~9/18/2025~~");
+            System.out.println("The highest grade so far is: " + highestSoFar);
+            System.out.println("The lowest grade so far is: " + lowestSoFar);
+            System.out.println("The average of the grades are: " + formatAvg);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found"); // Exception handling if file not found
+        }                                         // but it's in directory so it will always be there
     }
 }
