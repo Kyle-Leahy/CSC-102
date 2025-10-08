1a9965d879ebd48bb03a2a406accbdd388be9c5e
KyleLeahy <kll37@sunyorange.edu>
2025-10-08 10:52:17 -0400

    Lab4 is hella tricky.

--- src/Lab4/CheckingAccount.java
+++ src/Lab4/CheckingAccount.java
@@ -1,13 +1,16 @@
 package Lab4;
 
 public class CheckingAccount implements Banking{
-    public double checkBalance(){
-        return balance;
-    }
+    final double rate = 20.85; //$20 monthly fee charge adding up to ~$250/yr
+    public double checkBalance() {return balance;}
+    public double monthlyProcessing() {return rate;}
     public double withdraw(double amount){
-        amount = balance - amount;
+        balance = balance - amount;
         return amount;
     }
-
+    public double deposit(double amount){
+        balance = balance + amount;
+        return amount;
+    }
 
 }
