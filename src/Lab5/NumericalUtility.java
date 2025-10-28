1d5268d8bd061d2b86c0853a2942571c414021d5
Bjork <kyleleahy68@gmail.com>
2025-10-28 17:08:21 -0400

    Completely messed up the lab and realized I needed to remove all GUI etc. from methods. I knew it was kinda wrong, but it made sense because I felt lab 4 looked "messy". Also realized I was programming some of these are iterations instead of recursing, completely missing the point of the lab.

--- src/Lab5/NumericalUtility.java
+++ src/Lab5/NumericalUtility.java
@@ -1,85 +1,70 @@
 package Lab5;
-import javax.swing.JOptionPane;
 
-
+/**
+ * This class contains all methods for successfully completing Lab 5.
+ * @author Bjork / Kyle Leahy
+ * @version 2.0
+ */
 public class NumericalUtility {
-    public static void getFactorial(){
-        try {
-            double f = Double.parseDouble(JOptionPane.showInputDialog(null,
-                    "Please enter the number you want to get the factorial for." +
-                            "\n(All the numbers leading and including, multiplied to one another)"));
-            long factorial = 1;
-            if (f == 0 || f < 0) {
-                JOptionPane.showMessageDialog(null, "The factorial is 0 or negative.");
-            } else if (f == 1) {
-                JOptionPane.showMessageDialog(null, "The factorial is 1.");
-            }
-            for (int i = 1; i <= f; i++) {
-                factorial *= i;
-            }
-            JOptionPane.showMessageDialog(null, "The factorial is: " + factorial);
-        }catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Bro that's not even a number...");
-        }
-    }
-    public static void getNPowerM() {
-        try {
-            int n = Integer.parseInt(JOptionPane.showInputDialog(null,
-                    "A number raised to another of your choosing.\nEnter your base number below."));
-            int m = Integer.parseInt(JOptionPane.showInputDialog(null,
-                    "Enter the number you'd like " + n + " to be raised to: "));
-            JOptionPane.showMessageDialog(null, "You have created the number: " + Math.pow(n, m));
-        }catch(NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Why are you breaking this.");
-        }
-    }
-    public static void getSummation(){
-        try {
-            int answer = Integer.parseInt(JOptionPane.showInputDialog(null,
-                    "Enter the number you want summated\n(All numbers before and included, added together)"));
-            int sum = 0;
-            for (int i = 0; i <= answer; i++) {
-                sum += i;
-            }
-            JOptionPane.showMessageDialog(null, "The sum is: " + sum);
-        }catch(NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "That's not something to be summated");
-        }
-    }
-    private static int GCD(int first, int second){
-        if (second == 0){
-            return first; //Knew the base case but did some research on GCD, I was doing LCD at first.
-        } else  {
-            return GCD(second, first % second); // Euclidean Algorithm
-        }
-    }
-    public static void greatestCommonDivisor() {
-        try {
-            int firstNumber = Integer.parseInt(JOptionPane.showInputDialog(null, "This program is to find the greatest common divisor " +
-                    "for 2 different numbers of your choosing.\nEnter your first number below."));
-            int secondNumber = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter your second number below."));
-            int answer = GCD(firstNumber, secondNumber);
-            JOptionPane.showMessageDialog(null, "Your greatest common divisor is: " + answer);
-        }catch(NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "There's no way to find the GCD of that.");
-        }
-    }
-    private static long FIB(int n){
-        if (n <= 2) {return 1;}
+    /**
+     * Method to calculate a factorial from input.
+     * @param f The number received that will be turned into a factorial.
+     * @return The factorial of input 'f'.
+     */
+    public static long getFactorial(int f){
+        if (f == 0 || f == 1) {return 1;}
         else {
-            return FIB(n - 1) + FIB(n - 2);
+            return f * getFactorial(f - 1);
         }
     }
-    public static void getFibonacci(){
-        try {
-            int n = Integer.parseInt(JOptionPane.showInputDialog(null, "The famous Fibonacci sequence. " +
-                    "Enter a number, BUT this is recursive. \nBeware of high numbers as this is O(2^n) time complexity!"));
-            for (int i = 1; i <= n; i++) {
-                long FIB = FIB(i); //Followed the example given and analyzed it. Tried to make it one method but 2 are mandatory.
-                System.out.println("fib(" + i + ") = " + FIB); //Spamming windows of Java Swing would be annoying for user to click through
-            }
-        }catch(NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Fibonacci is confused by that input...");
+
+    /**
+     * Method to create a number from a base and exponent.
+     * @param n The base of the exponent to be made.
+     * @param m The exponent to the base.
+     * @return The exponent of input 'n' to the power of 'm'.
+     */
+    public static int getNPowerM(int n, int m) {
+        if (m == 0){return 1;}
+        else {
+            return n * getNPowerM(n,m-1);
         }
     }
+
+    /**
+     * Method to calculate a summation of all integers up to input 'sum'.
+     * @param sum The number to be summated.
+     * @return The sum of all numbers leading up to and including input 'sum'.
+     */
+    public static int getSummation(int sum){
+        if (sum == 0){return 0;}
+        else if (sum == 1){return 1;}
+        else {return sum + getSummation(sum - 1);}
+    }
+
+    /**
+     * Method to find the
+     * @param first First number to have the second one compared to.
+     * @param second Second number allowing the comparison to be calculated.
+     * @return The highest number that can be divided into both 'first' and 'second'.
+     */
+    public static int getGCD(int first, int second){
+        if (second == 0){return first;} //Knew the base case but did some research on getGCD, I was doing LCD at first.
+        else {
+            return getGCD(second, first % second); // Euclidean Algorithm
+        }
+    }
+
+    /**
+     * Method to calculate the number of the Fibonacci sequence at a given input.
+     * @param fib The number input to have a fibonacci sequence returned.
+     * @return The fibonacci sequence up to that number e.g. 6 = 8->(5 + 3).
+     */
+    public static long getFIB(int fib){
+        if (fib <= 2) {return 1;}
+        else {
+            return getFIB(fib - 1) + getFIB(fib - 2);
+        }
+    }
+
 }
--- src/Lab5/RecursionTester.java
+++ src/Lab5/RecursionTester.java
@@ -1,9 +1,16 @@
 package Lab5;
-
-
 import javax.swing.*;
 
+/**
+ * This class executes all NumericalUtility methods with menus and error handling at every step
+ * @author Bjork / Kyle Leahy
+ * @version 2.0
+ */
 public class RecursionTester {
+    /**
+     * Executes the main lab5 program calls.
+     * @param args The array of String arguments being fed into the stream
+     */
     public static void main(String[] args) {
         boolean flag = false;
         do {
@@ -13,18 +20,47 @@ public class RecursionTester {
                             "\nWelcome to the recursion tester!\n\nEnter the number of a menu choice:\n1.) Get a factorial" +
                             "\n2.) Create an exponent\n3.) Calculate a summation\n4.) Find the GCD of 2 numbers\n5.) Create a Fibonacci sequence" +
                             "\n-1 TO QUIT!"));
-                    if (menu == -1) {
-                        flag = true;
-                    }else if (menu == 1) {
-                        NumericalUtility.getFactorial();
+                    if (menu == -1) {flag = true;}
+                    else if (menu == 1) {
+                        try {
+                            int f = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number to find the factorial:"));
+                            JOptionPane.showMessageDialog(null, NumericalUtility.getFactorial(f));
+                        }catch(NumberFormatException e){
+                            JOptionPane.showMessageDialog(null, "Please enter a number.");
+                        }
                     }else if (menu == 2) {
-                        NumericalUtility.getNPowerM();
+                        try {
+                            int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number for the base of the exponent:"));
+                            int m = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number for the exponent:"));
+                            JOptionPane.showMessageDialog(null, NumericalUtility.getNPowerM(n,m));
+                        }catch(NumberFormatException e){
+                            JOptionPane.showMessageDialog(null, "Please enter a number.");
+                        }
                     }else if (menu == 3) {
-                        NumericalUtility.getSummation();
+                        try {
+                            int sum = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number to find the summation:"));
+                            JOptionPane.showMessageDialog(null, NumericalUtility.getSummation(sum));
+                        }catch(NumberFormatException e){
+                            JOptionPane.showMessageDialog(null, "Please enter a number.");
+                        }
                     }else if (menu == 4) {
-                        NumericalUtility.greatestCommonDivisor();
+                        try {
+                            int first = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the first number to find a GCD."));
+                            int second = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the second number to find a GCD with "+first+"."));
+                            JOptionPane.showMessageDialog(null, NumericalUtility.getGCD(first,second));
+                        }catch(NumberFormatException e){
+                            JOptionPane.showMessageDialog(null, "Please enter a number.");
+                        }
                     }else if (menu == 5) {
-                        NumericalUtility.getFibonacci();
+                        try {
+                            int fib = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number to find the fibonacci sequence:"));
+                            for (int i = 1; i <= fib; i++) {
+                                long f = NumericalUtility.getFIB(i);
+                                System.out.println("fib(" + i + ") = " + f);// Spamming JOptionPane windows would be annoying for the user lol.
+                            }
+                        }catch(NumberFormatException e){
+                            JOptionPane.showMessageDialog(null, "Please enter a number.");
+                        }
                     }else
                         JOptionPane.showMessageDialog(null, "Please enter the number of a menu choice!");
                 } while (!flag);
