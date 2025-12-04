c6f38b5e37caf23c0ca7e83dbc936d4bee00fffc
Bjork <kyleleahy68@gmail.com>
2025-12-04 18:56:56 -0500

    Transferring back to my laptop

--- src/Lab7/TreeSearchingLab.java
+++ src/Lab7/TreeSearchingLab.java
@@ -5,7 +5,7 @@ public class TreeSearchingLab {
         private static Node root;
         public BinaryTree() {root = null;}
         public BinaryTree(Comparable rootData, BinaryTree left, BinaryTree right) {
-            root = new Node<>();
+            root = new Node<>(rootData);
             root.data = rootData;
             root.left = left.root;
             root.right = right.root;
@@ -15,17 +15,31 @@ public class TreeSearchingLab {
             public T data;
             public Node<T> left;
             public Node<T> right;
-            public Node() {this.data = null;}
+            public Node(T data) {this.data = null;}
+
             public Node<T> add(T data) {
                 int compare = data.compareTo(this.data);
                 if(compare < 0){
-                    if(this.left == null){
-                        this.left = new Node<>(data);
+                    if(this.left == null){this.left = new Node<>(data);}
+                    else {left = left.add(data);}
+                } else if(compare > 0){
+                    if(this.right == null){this.right = new Node<>(data);}
+                    else{right = right.add(data);}
+                }
+                return this;
+            }
+
+            public Node<T> remove(T data){
+                int compare = data.compareTo(this.data);
+                if(compare < 0){
+                    if(this.left != null){this.left = left.remove(data);}
+                }
+                else if(compare > 0){
+                    if(this.right != null){this.right = right.remove(data);}
+                    else {
+                        if (this.left == null && this.right == null){return null;}
                     }
                 }
-            }
-            public void remove(){
-
             }
             public boolean isPresent(){ //Find
                 return data != null;
